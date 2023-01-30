d85276317e0f502e9f3170e750b5cf09cee75764
vietdung <vietdung282002@gmail.com>
2023-01-30 15:30:41 +0700

    commit

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from Constant import *
 from Solver import Solver
 
 pygame.font.init()
+font = pygame.font.SysFont("comicsans", 40)
 
 
 class Grid:
     def __init__(self, N, K, rows, cols, width, height):
         self.N = N
         self.K = K
-        self.solve = Solver(N,K)
+        self.solve = Solver(N, K)
         self.board = self.solve.board
         self.rows = rows
         self.cols = cols
@@ -19,6 +20,7 @@ class Grid:
         self.height = height
         self.Value = None
         self.selected = None
+        self.Btn_Sol = Button("Solve", 120, 50, (40, 545))
 
     def Solve(self):
         self.solve.solve(self.board.grid)
@@ -60,6 +62,8 @@ class Grid:
             for j in range(self.cols):
                 self.cubes[i][j].draw(screen)
 
+        self.Btn_Sol.draw(screen)
+
     def select(self, row, col):
         # Reset all other
         for i in range(self.rows):
@@ -72,14 +76,10 @@ class Grid:
     def clear(self):
         row, col = self.selected
         if self.cubes[row][col].value == 0:
-            self.cubes[row][col].flag = True
+            self.cubes[row][col].flag = 1
             self.cubes[row][col].set_temp(0)
 
     def click(self, pos):
-        """
-        :param: pos
-        :return: (row, col)
-        """
         if pos[0] < self.width and pos[1] < self.height:
             gap = self.width / 9
             x = pos[0] // gap
@@ -111,7 +111,6 @@ class Cubes:
         self.flag = None
 
     def draw(self, screen):
-        font = pygame.font.SysFont("comicsans", 40)
         gap = self.width / 9
         x = self.cols * gap
         y = self.rows * gap
@@ -125,10 +124,29 @@ class Cubes:
         if self.selected:
             pygame.draw.rect(screen, RED, (x, y, gap, gap), 3)
 
-        if self.flag is False:
-            pygame.draw.rect(screen,RED, pygame.Rect(x, y, gap, gap))
+        if self.flag == 0:
+            pygame.draw.rect(screen, RED, pygame.Rect(x, y, gap, gap))
             text = font.render(str(self.temp), 1, BLACK)
             screen.blit(text, (x + (gap / 2 - text.get_width() / 2), y + (gap / 2 - text.get_height() / 2)))
+            for i in range(self.rows + 1):
+                if i % 3 == 0 and i != 0:
+                    thick = 4
+                else:
+                    thick = 1
+                pygame.draw.line(screen, BLACK, (0, i * gap), (self.width, i * gap), thick)
+                pygame.draw.line(screen, BLACK, (i * gap, 0), (i * gap, self.height), thick)
+
+        if self.flag == 2:
+            pygame.draw.rect(screen, AQUA, pygame.Rect(x, y, gap, gap))
+            text = font.render(str(self.value), 1, BLACK)
+            screen.blit(text, (x + (gap / 2 - text.get_width() / 2), y + (gap / 2 - text.get_height() / 2)))
+            for i in range(self.rows + 1):
+                if i % 3 == 0 and i != 0:
+                    thick = 4
+                else:
+                    thick = 1
+                pygame.draw.line(screen, BLACK, (0, i * gap), (self.width, i * gap), thick)
+                pygame.draw.line(screen, BLACK, (i * gap, 0), (i * gap, self.height), thick)
 
     def set(self, val):
         self.value = val
@@ -140,8 +158,8 @@ class Cubes:
 def redraw_window(screen, board, time):
     screen.fill(WHITE)
     # Draw time
-    fnt = pygame.font.SysFont("comicsans", 35)
-    text = fnt.render("Time: " + timeToString(time), 1, BLACK)
+    font = pygame.font.SysFont("comicsans", 35)
+    text = font.render("Time: " + timeToString(time), 1, BLACK)
     screen.blit(text, (540 - 180, 560))
     # Draw grid and board
     board.draw(screen)
@@ -156,23 +174,53 @@ def timeToString(secs):
     return mat
 
 
+class Button:
+    def __init__(self, text, width, height, pos):
+
+        self.top_rect = pygame.Rect(pos, (width, height))
+        self.top_color = BTN_COLOR
+
+        self.text_surf = font.render(text, True, WHITE)
+        self.text_rect = self.text_surf.get_rect(center = self.top_rect.center)
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.top_color, self.top_rect, border_radius=8)
+        screen.blit(self.text_surf, self.text_rect)
+        self.check_click()
+
+    def check_click(self):
+        mouse_pos = pygame.mouse.get_pos()
+        if self.top_rect.collidepoint(mouse_pos):
+            if pygame.mouse.get_pressed()[0]:
+                return True
+
+
+
 class GameController(object):
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode(SCREENSIZE)
         pygame.display.set_caption("Sudoku")
-        self.board = Grid(N, K, 9, 9, SCREENWIDTH, SCREENHEIGHT-60)
+        self.board = Grid(N, K, 9, 9, SCREENWIDTH, SCREENHEIGHT - 60)
         self.val = None
         self.start = time.time()
+        self.play_time = time.time()
 
     def print(self):
         self.board.Solve()
+
+    def Solve(self):
+        for i in range(N):
+            for j in range(N):
+                if self.board.cubes[i][j].value == 0:
+                    self.board.cubes[i][j].flag = 2
+                    self.board.cubes[i][j].set(self.board.board.grid[i][j])
 
     def update(self):
         self.checkEvent()
 
     def checkEvent(self):
-        play_time = round(time.time() - self.start)
+        self.play_time = round(time.time() - self.start)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -203,10 +251,10 @@ class GameController(object):
                     i, j = self.board.selected
                     if self.board.cubes[i][j].temp != 0:
                         if self.board.place(self.board.cubes[i][j].temp):
-                            self.board.cubes[i][j].flag =True
+                            self.board.cubes[i][j].flag = 1
                             print("Success")
                         else:
-                            self.board.cubes[i][j].flag = False
+                            self.board.cubes[i][j].flag = 0
                             print("Wrong")
                         self.val = None
 
@@ -214,6 +262,8 @@ class GameController(object):
                             print("Game over")
 
             if event.type == pygame.MOUSEBUTTONDOWN:
+                if self.board.Btn_Sol.check_click():
+                    self.Solve()
                 pos = pygame.mouse.get_pos()
                 clicked = self.board.click(pos)
                 if clicked:
@@ -223,7 +273,7 @@ class GameController(object):
         if self.board.selected and self.val is not None:
             self.board.sketch(self.val)
 
-        redraw_window(self.screen, self.board, play_time)
+        redraw_window(self.screen, self.board, self.play_time)
         pygame.display.update()
 
 
